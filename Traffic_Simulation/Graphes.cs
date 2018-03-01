68168ac2c3ffe4238cf315a44e0332fa4e7847fe
kamilcglr <36273997+kamilcglr@users.noreply.github.com>
2018-03-01 15:00:26 +0100

    Séparation Graphes

---
+++ Traffic_Simulation/Graphes.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Wpf.Gauges;
+
+namespace Wpf.Gauges
+{
+    public partial class AngularGaugeExmple : UserControl, INotifyPropertyChanged
+    {
+        private double _value;
+
+        public AngularGaugeExmple()
+        {
+
+            Value = 0;
+
+            DataContext = this;
+        }
+
+        public double Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public void ChangeValueOnClick()
+        {
+            Value = new Random().Next(50, 250);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- Traffic_Simulation/MainWindow.xaml.cs
+++ Traffic_Simulation/MainWindow.xaml.cs
@@ -483,39 +483,3 @@ namespace Simulateur_0._0._2
 
 }
 
-namespace Wpf.Gauges { 
-    public partial class AngularGaugeExmple : UserControl, INotifyPropertyChanged
-    {
-        private double _value;
-
-        public AngularGaugeExmple()
-        {
-
-            Value = 0;
-
-            DataContext = this;
-        }
-
-        public double Value
-        {
-            get { return _value; }
-            set
-            {
-                _value = value;
-                OnPropertyChanged("Value");
-            }
-        }
-
-        public void ChangeValueOnClick()
-        {
-            Value = new Random().Next(50, 250);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName = null)
-        {
-            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-}
