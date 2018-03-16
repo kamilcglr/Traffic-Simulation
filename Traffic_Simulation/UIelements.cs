3550225811ffd27b4cae77067c9537a84e882724
kamilcglr <36273997+kamilcglr@users.noreply.github.com>
2018-03-16 12:21:55 +0100

    Nettoyage

--- Traffic_Simulation/UIelements.cs
+++ Traffic_Simulation/UIelements.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -16,10 +15,10 @@ namespace Simulateur_0._0._2
         public void Start(object sender, RoutedEventArgs e)
         {
             //----------------------INIT VALEURS-----------------------------
-            vitessemax = ((ChoixVitessemax.Value / 3.6) * 0.02) / 0.25;
+            vitessemax = ChoixVitessemax.Value / 3.6 * 0.02 / 0.25;
             acceleration = -0.002 * Math.Log(ChoixAcceleration.Value) + 0.0088;
             deceleration = ChoixDeceleration.Value;
-            _distanceEntreVehicule = (int)ChoixDistanceEntreVehicules.Value;
+            _distanceEntreVehicule = (int) ChoixDistanceEntreVehicules.Value;
             distancePtcritique = 100;
             distanceAnalyse = 500;
             //----------------------------------------------------------------
@@ -33,7 +32,7 @@ namespace Simulateur_0._0._2
             InitialiserLabelVmoy();
             InitialiserLabelNbVehiculesArret();
             //----------------------------------------------------------
-            
+
             if (Chargement) InitaliserVoitures();
 
             //--------------LANCEMENT TIMERs (à la fin !)---------------
@@ -42,18 +41,17 @@ namespace Simulateur_0._0._2
             _timer3.Start();
             _timerGauges.Start();
             //----------------------------------------------------------
-
-            
         }
+
         //----------------------------------GRAPHES----------------------------------------------
         public void InitialiserGraphVitesse()
         {
-
-            for (int i = 0; i < 20; i++) //Creation de 20 Points
+            for (var i = 0; i < 20; i++) //Creation de 20 Points
             {
                 var point = new ObservableValue(0);
                 VitesseValeurs.Add(point);
             }
+
             Graphtest.Series.Add(new LineSeries
             {
                 Values = new ChartValues<ObservableValue>
@@ -82,15 +80,15 @@ namespace Simulateur_0._0._2
                 PointGeometrySize = 0,
                 Fill = new SolidColorBrush(Color.FromRgb(77, 111, 150)),
                 StrokeThickness = 0
-               
             });
             Graphtest.HideTooltip();
             Graphtest.DataTooltip = null;
             Graphtest.Hoverable = false;
         }
+
         public void InitialiserGraphNbVehiculesArret()
         {
-            for (int i = 0; i < 20; i++) //Creation de 20 Points
+            for (var i = 0; i < 20; i++) //Creation de 20 Points
             {
                 var point = new ObservableValue(0);
                 NbVehiculesArretValeurs.Add(point);
@@ -122,26 +120,27 @@ namespace Simulateur_0._0._2
                     NbVehiculesArretValeurs[19]
                 },
                 Fill = new SolidColorBrush(Color.FromRgb(77, 111, 150))
-        });
+            });
             GraphNbVehiculesArret.HideTooltip();
             GraphNbVehiculesArret.DataTooltip = null;
             GraphNbVehiculesArret.Hoverable = false;
-
         }
+
         public void InitialiserGaugeNbvehiculesArret()
         {
             GaugeNbvehiculesArret.To = ChoixNombrevoitures.Value;
             GaugeNbvehiculesArret.GaugeActiveFill = new SolidColorBrush(Color.FromRgb(77, 111, 150));
         }
+
         public void InitialiserGaugeVitesse()
         {
             var vitesses = GaugeVitesse.ToValue;
             var decoupe = vitesses / 7;
 
             Sec1.FromValue = vitesses - decoupe;
-            Sec1.ToValue = (vitesses);
+            Sec1.ToValue = vitesses;
 
-            Sec2.FromValue =vitesses - decoupe*2;
+            Sec2.FromValue = vitesses - decoupe * 2;
             Sec2.ToValue = Sec1.FromValue;
 
             Sec3.FromValue = vitesses - decoupe * 3;
@@ -158,15 +157,16 @@ namespace Simulateur_0._0._2
 
             Sec7.FromValue = 0;
             Sec7.ToValue = Sec6.FromValue;
-
         }
+
         public void InitialiserHeatMap()
         {
-            for (int i = 0; i < 6; i++) //Creation de 20 Points
+            for (var i = 0; i < 6; i++) //Creation de 20 Points
             {
-                var point = new HeatPoint(i,0,0);
+                var point = new HeatPoint(i, 0, 0);
                 HeatMapValeurs1.Add(point);
             }
+
             HeatMap1.Series.Add(new HeatSeries
             {
                 Values = new ChartValues<HeatPoint>
@@ -177,21 +177,20 @@ namespace Simulateur_0._0._2
                     HeatMapValeurs1[3],
                     HeatMapValeurs1[4],
                     HeatMapValeurs1[5]
-
                 },
                 GradientStopCollection = new GradientStopCollection
                 {
-                    new GradientStop(Color.FromRgb(0,104,55), 0),
-                    new GradientStop(Color.FromRgb(0,104,55), .10),
-                    new GradientStop(Color.FromRgb(26,152,80), .20),
-                    new GradientStop(Color.FromRgb(102,189,99), .30),
-                    new GradientStop(Color.FromRgb(166,217,106), .40),
-                    new GradientStop(Color.FromRgb(217,239,139), .50),
-                    new GradientStop(Color.FromRgb(254,200,139), .60),
-                    new GradientStop(Color.FromRgb(253,174,97), .70),
-                    new GradientStop(Color.FromRgb(244,109,67), .80),
-                    new GradientStop(Color.FromRgb(215,48,39), .90),
-                    new GradientStop(Color.FromRgb(165,0,38), 1)
+                    new GradientStop(Color.FromRgb(0, 104, 55), 0),
+                    new GradientStop(Color.FromRgb(0, 104, 55), .10),
+                    new GradientStop(Color.FromRgb(26, 152, 80), .20),
+                    new GradientStop(Color.FromRgb(102, 189, 99), .30),
+                    new GradientStop(Color.FromRgb(166, 217, 106), .40),
+                    new GradientStop(Color.FromRgb(217, 239, 139), .50),
+                    new GradientStop(Color.FromRgb(254, 200, 139), .60),
+                    new GradientStop(Color.FromRgb(253, 174, 97), .70),
+                    new GradientStop(Color.FromRgb(244, 109, 67), .80),
+                    new GradientStop(Color.FromRgb(215, 48, 39), .90),
+                    new GradientStop(Color.FromRgb(165, 0, 38), 1)
                     /*
                      new GradientStop(Color.FromRgb(0,104,55), 0),
                     new GradientStop(Color.FromRgb(0,104,55), .10),
@@ -207,13 +206,13 @@ namespace Simulateur_0._0._2
                     */
                 },
                 DrawsHeatRange = false
-
             });
-            for (int i = 0; i < 5; i++) //Creation de 20 Points
+            for (var i = 0; i < 5; i++) //Creation de 20 Points
             {
                 var point = new HeatPoint(i, 1, 0);
                 HeatMapValeurs2.Add(point);
             }
+
             HeatMap1.Series.Add(new HeatSeries
             {
                 Values = new ChartValues<HeatPoint>
@@ -246,28 +245,28 @@ namespace Simulateur_0._0._2
                     new GradientStop(Color.FromRgb(165,0,38), 1)
                      */
                 },
-                DrawsHeatRange = false,
+                DrawsHeatRange = false
             });
             HeatMap1.HideTooltip();
             HeatMap1.DataTooltip = null;
             HeatMap1.Hoverable = false;
         }
+
         public void InitaliserVoitures()
         {
             // On effectue cette étape si c'est la première fois qu'est pressé le bouton
             {
                 var nbvoituresVoiegauche =
-                    (int)(ChoixNombrevoitures.Value * (ChoixProportionVoituregauche.Value / 100));
-                var nbvoituresVoiedroite = (int)ChoixNombrevoitures.Value - nbvoituresVoiegauche;
-                var densiteCamion = (int)ChoixDensitecamion.Value;
-                _distanceEntreVehicule = (int)ChoixDistanceEntreVehicules.Value;
+                    (int) (ChoixNombrevoitures.Value * (ChoixProportionVoituregauche.Value / 100));
+                var nbvoituresVoiedroite = (int) ChoixNombrevoitures.Value - nbvoituresVoiegauche;
+                var densiteCamion = (int) ChoixDensitecamion.Value;
+                _distanceEntreVehicule = (int) ChoixDistanceEntreVehicules.Value;
 
                 var i = nbvoituresVoiegauche;
                 var decoupageL1 = 800;
-                if (i != 0) { decoupageL1 = 800 / i; }
+                if (i != 0) decoupageL1 = 800 / i;
                 while (i != 0)
                 {
-
                     var voiture = new Voiture();
                     if (_rand.Next(100) < densiteCamion && Chargement
                     ) //Pas besoin de faire la densité de camion pour les autres cas
@@ -296,35 +295,32 @@ namespace Simulateur_0._0._2
                     voiture.Lane = 1;
                     voiture.Vitesse = vitessemax;
                     voiture.Yposition = PositionL1;
-                    voiture.Xposition = (decoupageL2 * j - 800);
+                    voiture.Xposition = decoupageL2 * j - 800;
                     Cars.Add(voiture);
                     Affichage.Children.Add(voiture);
                     Canvas.SetLeft(voiture, voiture.Xposition);
                     Canvas.SetBottom(voiture, voiture.Yposition);
                     j--;
                 }
+
                 Chargement = false;
             }
         }
+
         public void InitialiserLabelNbVehiculesArret()
         {
-            for (int i = 0; i < 120; i++)
-            {
-                Nbarret.Add(0);
-            }
+            for (var i = 0; i < 120; i++) Nbarret.Add(0);
         }
+
         public void InitialiserLabelVmoy()
         {
-            for (int i = 0; i < 120; i++)
-            {
-                Vmoy.Add(ChoixVitessemax.Value);
-            }
+            for (var i = 0; i < 120; i++) Vmoy.Add(ChoixVitessemax.Value);
         }
 
         public void InitialiserVmoyFctV()
         {
-            
         }
+
         private void Ajoutcamion(Voiture voiture)
         {
             voiture.Vehiculelent = true;
@@ -333,6 +329,7 @@ namespace Simulateur_0._0._2
             voiture.Width = 32;
             voiture.Height = 10;
         }
+
         private void Choix_vitesse_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             VitessemaxChoixAffichage.Content = "Vitesse max : " + Math.Round(ChoixVitessemax.Value, 0) + "km/h";
@@ -340,6 +337,7 @@ namespace Simulateur_0._0._2
             GaugeVitesse.ToValue = ChoixVitessemax.Value + 20;
             InitialiserGaugeVitesse();
         }
+
         private void Choix_acceleration_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             //Acceleration renvoyée en pixel/20ms2
@@ -347,27 +345,32 @@ namespace Simulateur_0._0._2
             AccelerationmaxChoixAffichage.Content =
                 "Accélération max (0 à 100) : " + Math.Round(ChoixAcceleration.Value, 1) + "s";
         }
+
         private void Choix_proportion_voituregauche_ValueChanged(object sender,
             RoutedPropertyChangedEventArgs<double> e)
         {
             ProportionVoiegaucheChoixAffichage.Content = "Proportion véhicules file de gauche" +
                                                          ChoixProportionVoituregauche.Value.ToString("F0") + " %";
         }
+
         private void choix_distance_entre_vehicules_ValueChanged(object sender,
             RoutedPropertyChangedEventArgs<double> e)
         {
             DistanceSecuriteAffichage.Content =
                 "Distance entre veh : " + ChoixDistanceEntreVehicules.Value.ToString("F0");
         }
+
         private void choix_deceleration_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             DecelerationChoixAffichage.Content = "Deceleration : " + Math.Round(ChoixDeceleration.Value, 3);
         }
+
         private void Choix_densitecamion_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             DensitecamionChoixAffichage.Content =
                 "Proportion de camions " + ChoixDensitecamion.Value.ToString("F0") + " %";
         }
+
         private void Choix_nombrevoitures_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             NombrevehiculesChoixAffichage.Content =
